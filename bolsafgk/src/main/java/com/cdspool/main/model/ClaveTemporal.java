67559a113b85f69cae8b8c28e803307a188045e9
Thalia Campos <51869261+1-419937@users.noreply.github.com>
2019-11-13 14:15:44 -0600

    Crud ClaveTemporal

---
+++ bolsafgk/src/main/java/com/cdspool/main/model/ClaveTemporal.java
@@ -0,0 +1,62 @@
+package com.cdspool.main.model;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="clave_temporal")
+public class ClaveTemporal {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+    //Entidades
+	private Integer id;
+	private String clavet;
+	private String estado;
+	
+	@JoinColumn(name="id_usuario")
+	@ManyToOne(optional = false, fetch = FetchType.EAGER)
+	private Usuarios usuario;
+
+	//Los Get y Set
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getClavet() {
+		return clavet;
+	}
+
+	public void setClavet(String clavet) {
+		this.clavet = clavet;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public Usuarios getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuarios usuario) {
+		this.usuario = usuario;
+	}
+	
+	
+}
---
+++ bolsafgk/src/main/java/com/cdspool/main/repository/IClaveTeRepository.java
@@ -0,0 +1,11 @@
+package com.cdspool.main.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cdspool.main.model.ClaveTemporal;
+
+@Repository
+public interface IClaveTeRepository extends CrudRepository<ClaveTemporal, Integer> {
+
+}
