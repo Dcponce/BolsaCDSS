ccb494641e99c3379803e860e99971590b342231
Dcponce <50255577+Dcponce@users.noreply.github.com>
2019-12-06 16:58:06 -0600

    seguimiento de consumo controlador email

--- assets/js/email/accesoEmail.js
+++ assets/js/email/accesoEmail.js
@@ -1,13 +1,41 @@
-$(document).ready(function (){
+$(document).ready(function () {
 
-var uri="http://localhost:8080/email";
+    var uri = "http://localhost:8080/email";
+
+    listar(uri);
+    leer();
 
 });
 
-function listar(uri){
-    $.getJSON(uri, function(data){
-        if(data !=null){
-            
+function listar(uri) {
+    $.getJSON(uri, function (data) {
+        if (data != null) {
+
+            $('#tabla>tbody').empty();
+            var fila = "";
+            $.each(data, function (i, v) {
+                fila = "<tr>" +
+                    "<td>" + v.asunto + "</td>" +
+                    "<td>" + v.emisor.nombre + "</td>" +
+                    "<td>" + v.receptor.nombre + "</td>" +
+                    "<td>" + v.contenido + "</td>" +
+                    "<td> <a href='#' onclick='leer(" + v.id + ")'>Seleccionar</td>" +
+                    "<td> Eliminar </td>" +
+                    "</tr>";
+
+                $('#tabla>tbody').append(fila);
+            });
         }
     });
+}
+
+function leer(id) {
+    $.getJSON("http://localhost:8080/email/correos/" + id, function (data) {
+        if (data != null) {
+            $('#emi').text(data.emisor.nombre);
+            $('#rece').text(data.receptor.nombre);
+            $('#asu').text(data.contenido);
+        }
+    });
+
 }
