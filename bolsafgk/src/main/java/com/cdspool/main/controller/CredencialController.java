188d7e027830b47979e95b5d0f5558696dc0b2e8
Thalia Campos <51869261+1-419937@users.noreply.github.com>
2019-12-06 17:03:40 -0600

    Modificacion Credencial

--- bolsafgk/src/main/java/com/cdspool/main/controller/CredencialController.java
+++ bolsafgk/src/main/java/com/cdspool/main/controller/CredencialController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cdspool.main.model.Credencial;
@@ -19,13 +21,14 @@ import com.cdspool.main.repository.ICredencialRepository;
 
 @RestController
 @RequestMapping(value = "credencial")
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class CredencialController {
 	
 	@Autowired 
 	ICredencialRepository iCred;
 	
 	@GetMapping("/lista")
-	@Secured("ROLE_ALUMNO")
+	//@Secured("ROLE_ALUMNO")
 	public List<Credencial> lista(){
 		return (List<Credencial>) iCred.findAll();
 	}
