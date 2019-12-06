3d46f3036297eb77536a5e55b75d7bc02ab98594
Dcponce <50255577+Dcponce@users.noreply.github.com>
2019-12-06 10:44:45 -0600

    vistas

--- assets/js/email/accesoEmail.js
+++ assets/js/email/accesoEmail.js
@@ -1,3 +1,13 @@
 $(document).ready(function (){
+
 var uri="http://localhost:8080/email";
+
 });
+
+function listar(uri){
+    $.getJSON(uri, function(data){
+        if(data !=null){
+            
+        }
+    });
+}
