3ae05d3333e677a1f70cf7ea2fdc498d24ec32bd
Dcponce <dcponce22@gmail.com>
2019-12-10 23:33:28 -0600

    Secciones de educación,habilidades,proyectos

--- assets/js/alumno/accesoAlumno.js
+++ assets/js/alumno/accesoAlumno.js
@@ -2,7 +2,13 @@ $(document).ready(function () {
 
     var uri = "http://localhost:8080/alumnos";
     var uriD = "http://localhost:8080/municipios";
-    
+    var uriC = "http://localhost:8080/certificacion";
+    var uriH = "http://localhost:8080/habilidades";
+
+    getDepto(uriD, 0);
+    getCert(uriC, 0)
+    getHabi(uriH, 0)
+
     $('#datosP').on('click', function () {
         nuevo(base_uri);
     });
@@ -15,60 +21,60 @@ $(document).ready(function () {
 
 });
 
-function guardar(uri){
-    var id=$('#id').val();
-    var nom=$('#nombre').val();
-    var apellido=$('#apellido').val();
-    var tel=$('#telefono').val();
-    var cel=$('#celular').val();
-    var dir=$('#direccion').val();
-    var pro=$('#proyeto').val();
-    var nac=$('#nacimiento').val();
-    var muni=$('#municipio').val();
+function guardar(uri) {
+    var id = $('#id').val();
+    var nom = $('#nombre').val();
+    var apellido = $('#apellido').val();
+    var tel = $('#telefono').val();
+    var cel = $('#celular').val();
+    var dir = $('#direccion').val();
+    var pro = $('#proyeto').val();
+    var nac = $('#nacimiento').val();
+    var muni = $('#municipio').val();
     var metodo = "POST";
     var accion = "Guardado";
 
-    if(id > 0){
+    if (id > 0) {
         metodo = "PUT";
-        accion="Actualizado";
-    }else{
-        id=null;
+        accion = "Actualizado";
+    } else {
+        id = null;
     }
 
-    if(sede !="" && muni >0){
+    if (sede != "" && muni > 0) {
 
-        var data={
-            "id":id,
-            "nombre":nom,
-            "apellido":apellido,
-            "telefono":tel,
-            "celular":cel,
-            "direccion":dir,
-            "proyecto":pro,
-            "fecha":nac,
-            "id_municipio":{
-                "id":muni
+        var data = {
+            "id": id,
+            "nombre": nom,
+            "apellido": apellido,
+            "telefono": tel,
+            "celular": cel,
+            "direccion": dir,
+            "proyecto": pro,
+            "fecha": nac,
+            "id_municipio": {
+                "id": muni
             },
-            "id_usuario":{
-                "id":id
+            "id_usuario": {
+                "id": id
             }
         };
 
         $.ajax({
-            url:uri,
-            method:metodo,
+            url: uri,
+            method: metodo,
             contentype: "application/json",
             data: JSON.stringify(data),
-            success: function(){
+            success: function () {
                 $('#educacion').on('click', function () {
-                    nuevo(uri); 
+                    nuevo(uri);
                 });
             }
         });
     }
 }
 
-function getDepto(uriD,id) {
+function getDepto(uriD, id) {
 
     $.getJSON(uriD, function (data) {
         if (data != null) {
@@ -91,7 +97,7 @@ function getDepto(uriD,id) {
 }
 
 function getMunicipioByDepto(uriD, idDepto, id) {
-    $.getJSON(uriD +"/muni/"+ idDepto, function (data) {
+    $.getJSON(uriD + "/muni/" + idDepto, function (data) {
         if (data != null) {
             $('#municipio').empty();
             $('#municipio').append("<option selected disabled>Seleccione un municipio</option>");
@@ -105,6 +111,48 @@ function getMunicipioByDepto(uriD, idDepto, id) {
                 }
 
                 $('#municipio').append(fila);
+            });
+        }
+    });
+}
+
+function getCert(uriC, id) {
+
+    $.getJSON(uriC, function (data) {
+        if (data != null) {
+
+            $('#certificacion').empty();
+            $('#certificacion').append("<option selected disabled>Seleccione su certificación</option>");
+            var fila = "";
+            $.each(data, function (i, v) {
+
+                if (v.id == id) {
+                    fila = '<option value="' + v.id + '" selected>' + v.nombre + '</option>';
+                } else {
+                    fila = '<option value="' + v.id + '">' + v.nombre + '</option>';
+                }
+                $('#certificacion').append(fila);
+            });
+        }
+    });
+}
+
+function getHabi(uriH, id) {
+
+    $.getJSON(uriH, function (data) {
+        if (data != null) {
+
+            $('#habilidad').empty();
+            $('#habilidad').append("<option selected disabled>Seleccione sus habilidades</option>");
+            var fila = "";
+            $.each(data, function (i, v) {
+
+                if (v.id == id) {
+                    fila = '<option value="' + v.id + '" selected>' + v.descripcion + '</option>';
+                } else {
+                    fila = '<option value="' + v.id + '">' + v.descripcion + '</option>';
+                }
+                $('#habilidad').append(fila);
             });
         }
     });
