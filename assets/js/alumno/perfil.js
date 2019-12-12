3babc98cb21d4f1d2f4915be4687f47b9a68bee6
Dcponce <50255577+Dcponce@users.noreply.github.com>
2019-12-12 16:48:37 -0600

    Carga de cv

--- assets/js/alumno/perfil.js
+++ assets/js/alumno/perfil.js
@@ -3,10 +3,30 @@ $(document).ready(function () {
     var uriDoc = "http://localhost:8080/document/oupload";
 
     $('#perfil').on('click', function () {
-        cargarImg(uriDt);
+        cargarImg(uriDoc);
     });
 });
 
 function cargarImg(uriDoc) {
+    var name = $('#fileName').val();
 
+    var midata = new FormData();
+    midata.append("file", name);
+    $.ajax({
+
+        url: uriDoc,
+        headers: {
+            'Authorization': JSON.parse(localStorage.getItem('Token'))
+        },
+        contentType: "application/json",
+        data: midata,
+        processData: false,
+        cache: false,
+        success: function () {
+            alert("Cargar exitosa");
+        },
+        error:function(error){
+            alert(error);
+        }
+    });
 }
