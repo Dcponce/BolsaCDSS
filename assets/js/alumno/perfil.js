bf9ee186f3c15b6cf0aa18badb65cabaffed9e20
Dcponce <50255577+Dcponce@users.noreply.github.com>
2019-12-12 16:25:03 -0600

    -actulizacion

---
+++ assets/js/alumno/perfil.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    var uriDoc = "http://localhost:8080/document/oupload";
+
+    $('#perfil').on('click', function () {
+        cargarImg(uriDt);
+    });
+});
+
+function cargarImg(uriDoc) {
+
+}
