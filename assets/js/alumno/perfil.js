308ea4065fb9ed5d48ae518a9b6f7491f4b39ecd
Nehemias Sandoval <nehe.sandovalfgkss@sv.cds>
2019-12-13 07:40:43 -0600

    MERGE

--- assets/js/alumno/perfil.js
+++ assets/js/alumno/perfil.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    var uriDoc = "http://localhost:8080/document/oupload";
+    var uriDoc = "http://localhost:8080/documento/upload";
 
     $('#perfil').on('click', function () {
         cargarImg(uriDoc);
@@ -18,6 +18,7 @@ function cargarImg(uriDoc) {
         headers: {
             'Authorization': JSON.parse(localStorage.getItem('Token'))
         },
+        method:"POST",
         contentType: "application/json",
         data: midata,
         processData: false,
