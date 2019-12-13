cce6712a320b05673430af91672a3a4aaaa0eef4
Thalia Campos <51869261+1-419937@users.noreply.github.com>
2019-12-13 17:00:27 -0600

    empresa

---
+++ assets/js/empresa/accesoempresa.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+
+    var base_uri = "http://localhost:8080/empresa";
+
+    $('#nuevo').on('click', function () {
+        nuevo(base_uri);
+    });
+
+
+});
+
+function nuevo(base_uri) {
+    var idc = $('#id').val();
+    var nombre = $('#nombre').val();
+    var telefono = $('#telefono').val();
+    var metodo = "POST";
+    var accion = "Guardado";
+
+    if (id > 0) {
+        metodo = "PUT";
+        accion = "Actualizado";
+    } else {
+        id = null;
+    }
+
+    if (nombre != null) {
+
+        var data = {
+            "id": id,
+            "nombre": nombre,
+            "telefono": telefono,
+            "id_usuario": {
+                "id": idc
+            }
+        };
+
+        $.ajax({
+            url: base_uri,
+            method: metodo,
+            contentType: "application/json",
+            data: JSON.stringify(data),
+            success: function () {
+                alert("Registro agregar Existosamente !!!");
+            }
+        });
+    }
+}
+
