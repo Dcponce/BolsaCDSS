bd1248b2f331e972df7d4e8357ad1233053642ae
nehe.sandovalfgkss <nehe.sandovalfgkss@CDS-SS-2011-006.sv.cds>
2019-12-23 16:12:14 -0600

    Query filtro

--- bolsafgk/src/main/java/com/cdspool/main/repository/IAlumnoRepository.java
+++ bolsafgk/src/main/java/com/cdspool/main/repository/IAlumnoRepository.java
@@ -1,13 +1,23 @@
 package com.cdspool.main.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.cdspool.main.model.Alumno;
 import com.cdspool.main.model.Usuario;
 
 @Repository
-public interface IAlumnoRepository extends CrudRepository<Alumno, Integer>{
+public interface IAlumnoRepository extends CrudRepository<Alumno, Integer> {
 
-	public Alumno findByUsuario (Usuario usuario);
+	@Query(
+			value = "SELECT a FROM Alumno a INNER JOIN Usuario u ON u.id = a.id_usuario INNER JOIN Educacion e ON e.id_usuario = u.id INNER JOIN Municipio m ON a.id_municipio = m.id INNER JOIN DetalleHabilidades dh ON dh.id_usuario = u.id WHERE (e.certificacion = :certi OR m.departamento = :depto OR dh.habilidad IN (:habi)) GROUP BY 1 ORDER BY 1;", 
+			nativeQuery = true)
+	List<Alumno> findAlumnoByCertificacionOrDepartamentoOrHabilidadNamedParamsNative(@Param("certi") Integer certi, @Param("depto") Integer depto, @Param("habilidad") String habi);
+
+	public Alumno findByUsuario(Usuario usuario);
+
 }
--- bolsafgk/src/main/java/com/cdspool/main/service/AlumnoService.java
+++ bolsafgk/src/main/java/com/cdspool/main/service/AlumnoService.java
@@ -8,8 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cdspool.main.model.Alumno;
+import com.cdspool.main.model.Habilidad;
 import com.cdspool.main.model.Usuario;
 import com.cdspool.main.repository.IAlumnoRepository;
+import com.cdspool.main.repository.ICertificacionRepository;
+import com.cdspool.main.repository.IDepartamentosRepository;
+import com.cdspool.main.repository.IHabilidadRepository;
 import com.cdspool.main.repository.IUsuarioRepository;
 
 @Service
@@ -18,6 +22,15 @@ public class AlumnoService {
 
 	@Autowired
 	IAlumnoRepository iAlumnos;
+	
+	@Autowired
+	IHabilidadRepository rHabi;
+
+	@Autowired
+	IDepartamentosRepository rDepto;
+
+	@Autowired
+	ICertificacionRepository rCerti;
 
 	@Autowired
 	IUsuarioRepository iUsuario;
@@ -41,5 +54,28 @@ public class AlumnoService {
 	public Alumno findByUsuario(Integer id) {
 		Usuario user = iUsuario.findById(id).get();
 		return iAlumnos.findByUsuario(user);
+	}
+
+	public List<Alumno> filter(Integer depto, Integer certi, Integer[] habilidades) {
+
+		//Certificacion cer = rCerti.findById(certi).get();
+		//Departamentos depa = rDepto.findById(depto).get();
+
+		String habilidad = "";
+		for (Integer i = 0; i < habilidades.length; i++) {
+			Habilidad habi = rHabi.findById(habilidades[i]).get();
+
+			if (habi != null) {
+
+				if (habilidades.length == i) {
+					habilidad += habi.getId();
+				} else {
+					habilidad += habi.getId() + ",";
+				}
+			}
+		}
+		
+		return iAlumnos.findAlumnoByCertificacionOrDepartamentoOrHabilidadNamedParamsNative(certi, depto, habilidad);
+
 	}
 }
