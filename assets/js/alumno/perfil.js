8159f08552d6a5a2a1dddb5b0e746b276e224403
Dcponce <dcponce22@gmail.com>
2019-12-30 18:25:46 -0600

    alert corregido al cargar cv

--- assets/js/alumno/perfil.js
+++ assets/js/alumno/perfil.js
@@ -35,7 +35,7 @@ function cargarImg() {
             if (result.value) {
                 location.reload();
             }
-        })
+        });
     }).fail(function () {
         Swal.fire({
             icon: 'error',
@@ -66,7 +66,13 @@ function cargarCv() {
         Swal.fire({
             icon: 'success',
             title: 'Excelente',
-            text: 'Archivo cargado con exito'
+            text: 'Archivo cargado con exito',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Ok'
+        }).then((result) => {
+            if (result.value) {
+                location.reload();
+            }
         });
     }).fail(function () {
         Swal.fire({
