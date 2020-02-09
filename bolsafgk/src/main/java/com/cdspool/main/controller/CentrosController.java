690f231d68b354c3a3a9d503430fb2424665c14a
Dcponce <dcponce22@gmail.com>
2020-02-08 18:40:19 -0600

    -Mantenimiento para centros de desarrollo

---
+++ bolsafgk/src/main/java/com/cdspool/main/controller/CentrosController.java
@@ -0,0 +1,53 @@
+package com.cdspool.main.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cdspool.main.model.Centros;
+import com.cdspool.main.repository.ICentrosRepository;
+
+@RestController
+@RequestMapping(value = "centros")
+public class CentrosController {
+
+	@Autowired
+	ICentrosRepository rCentros;
+	
+	//Listar centros cds
+	@GetMapping
+	@Secured({ "ROLE_ALUMNO", "ROLE_ADMIN" })
+	public List<Centros> lista(){
+		return (List<Centros>) rCentros.findAll();
+	}
+	
+	//Eliminar centros
+	@DeleteMapping("/{id}")
+	@Secured("ROLE_ADMIN")
+	public void delete (@PathVariable Integer id) {
+		rCentros.deleteById(id);
+	}
+	
+	//Ingresar centros
+	@PostMapping
+	@Secured("ROLE_ADMIN")
+	public void save(@RequestBody Centros centros) {
+		rCentros.save(centros);
+	}
+	
+	//Editar centros
+	@PutMapping
+	@Secured("ROLE_ADMIN")
+	public void update(@RequestBody Centros centros) {
+		rCentros.save(centros);
+	}
+}
